a457708a4302fa710606437a56ffebfebd8f9250
James Lee <zging275@gmail.com>
2024-09-26 17:26:57 +0900

    All Edit

--- sbp-gallery.js
+++ sbp-gallery.js
@@ -72,3 +72,4 @@ class SBPGallery {
     }
   }
 }
+// Add image Gallery
--- scripts/main.js
+++ scripts/main.js
@@ -116,3 +116,5 @@ document.addEventListener("DOMContentLoaded", function () {
       item.classList.add("active");
     }
   });
+
+  // Add Image Gallery
